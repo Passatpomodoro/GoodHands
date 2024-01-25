4be3a887d7651d967d045f0833b1352cc2deab23
przemekwieczorek <wieszczorekk@gmail.com>
2024-01-25 18:23:52 +0100

    dodanie bara - kompletny widok home/start

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -26,23 +26,23 @@ export default function Home () {
                     </div>
                 </div>
             </div>
-            <div>
-                <div>
-                    <div>
+            <div className="home-columns">
+                <div className="home-columns-flex">
+                    <div className="home-columns-content">
                         <h2>10</h2>
                         <h3>ODDANYCH WORKÓW</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
                     </div>
                 </div>
-                <div>
-                    <div>
+                <div className="home-columns-flex">
+                    <div className="home-columns-content">
                         <h2>5</h2>
                         <h3>WSPARTYCH ORGANIZACJI</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
                     </div>
                 </div>
-                <div>
-                    <div>
+                <div className="home-columns-flex">
+                    <div className="home-columns-content">
                         <h2>7</h2>
                         <h3>ZORGANIZOWANY ZBIÓREK</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
