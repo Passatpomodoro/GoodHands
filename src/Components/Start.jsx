40e48c0f276b0feb2480309c9c058b1f649028bd
przemekwieczorek <wieszczorekk@gmail.com>
2024-01-25 13:24:16 +0100

    dodanie assets

--- src/Components/Start.jsx
+++ src/Components/Start.jsx
@@ -1,15 +1,10 @@
-import { Link } from 'react-router-dom';
-import Navigation from "./Navigation.jsx";
-
 
 export default function Start () {
     return (
         <>
-        <div>
-            <button><Link to="/login">Zaloguj</Link></button>
-            <button><Link to="/signin">Załóż konto</Link></button>
-        </div>
-        <Navigation/>
+            <div id="start">
+                <h1>SKRRRRT</h1>
+            </div>
         </>
     )
 }
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -3,9 +3,16 @@ import AllAbout from "../Components/AllAbout.jsx";
 import AboutUs from "../Components/AboutUs.jsx";
 import FoundationAndOrganizations from "../Components/FoundationAndOrganizations.jsx";
 import Contact from "../Components/contact.jsx";
+import {Link} from "react-router-dom";
+import Navigation from "../Components/Navigation.jsx";
 export default function Home () {
     return(
         <>
+            <div className="home-log-buttons">
+                <button><Link to="/login">Zaloguj</Link></button>
+                <button><Link to="/signin">Załóż konto</Link></button>
+            </div>
+            <Navigation/>
             <Start/>
             <AllAbout/>
             <AboutUs/>
