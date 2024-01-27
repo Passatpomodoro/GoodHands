edab81f7023c7a5107e00d1e90fd4f747da87913
przemekwieczorek <wieszczorekk@gmail.com>
2024-01-27 15:26:41 +0100

    zaczęty ekran logowania

--- src/Views/LogIn.jsx
+++ src/Views/LogIn.jsx
@@ -1,7 +1,23 @@
+import LogButtons from "../Components/LogButtons.jsx";
+import Navigation from "../Components/Navigation.jsx";
+
 export default function LogIn () {
     return (
         <>
-        <h1>SKRRRRT</h1>
+            <LogButtons/>
+            <Navigation/>
+        <div>
+            <h1>Zaloguj się</h1>
+            <img src="/src/assets/Decoration.svg"/>
+            <div>
+                <input/>
+                <input/>
+            </div>
+            <div>
+                <button>Załóż konto</button>
+                <button>Zaloguj się</button>
+            </div>
+        </div>
         </>
     )
 }
